0231cba17053f9fb675092ffc13c0aa55f84bd2b
Danil Sidoruk <patriotrossii2019@mail.ru>
2022-10-20 13:26:19 +0300

    Add unique constraint on login

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA service;
 
 CREATE TABLE IF NOT EXISTS service.users(
     id SERIAL NOT NULL, full_name VARCHAR NOT NULL,
-    login VARCHAR NOT NULL, password VARCHAR NOT NULL
+    login VARCHAR UNIQUE NOT NULL, password VARCHAR NOT NULL
 );
 
 INSERT INTO service.users(full_name, login, password) VALUES(
